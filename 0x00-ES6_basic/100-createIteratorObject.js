a1325659cfb26e33c1d26a73ffd453ff5d5142b5
kaluogba <kaluogba1990@gmail.com>
2024-04-22 14:42:33 -0700

    ES6

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,26 @@
+export default function createIteratorObject(report) {
+  const departments = Object.values(report.allEmployees);
+  let currentDepartmentIndex = 0;
+  let currentEmployeeIndex = 0;
+
+  return {
+    [Symbol.iterator]() {
+      return this;
+    },
+    next() {
+      if (currentDepartmentIndex < departments.length) {
+        const currentDepartment = departments[currentDepartmentIndex];
+        if (currentEmployeeIndex < currentDepartment.length) {
+          const employee = currentDepartment[currentEmployeeIndex];
+          currentEmployeeIndex++;
+          return { value: employee, done: false };
+        } else {
+          currentDepartmentIndex++;
+          currentEmployeeIndex = 0;
+          return this.next();
+        }
+      }
+      return { done: true };
+    },
+  };
+}
---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,12 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let result = '';
+  let iterator = reportWithIterator.next();
+  while (!iterator.done) {
+    result += iterator.value;
+    iterator = reportWithIterator.next();
+    if (!iterator.done) {
+      result += ' | ';
+    }
+  }
+  return result;
+}
